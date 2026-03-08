677d8bef8645803b1692d56949a51d1d9570775f
Federico Miras <miras.federico@gmail.com>
2026-03-08 18:14:28 -0300

    Initial Commit

---
+++ VibeDesign/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibeDesign
+//
+//  Created by Federico Miras on 08/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibeDesign/VibeDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeDesignApp.swift
+//  VibeDesign
+//
+//  Created by Federico Miras on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VibeDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
